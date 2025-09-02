76945539c0630efc3191a116ab8bccd852d9c53f
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-09-02 18:56:08 +0100

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Teevil is working fine';
+    return 'Tap Sync is working fine';
   }
 }
