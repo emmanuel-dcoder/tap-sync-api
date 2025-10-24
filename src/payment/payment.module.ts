63214cf3349482b683defd4a2393be1673e0e478
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-10-24 09:49:45 +0100

    feat: payment fixes

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -9,12 +9,14 @@ import {
 import { TransactionService } from 'src/transaction/services/transaction.service';
 import { OrderService } from 'src/order/services/order.service';
 import { Order, OrderSchema } from 'src/order/schemas/order.schema';
+import { User, UserSchema } from 'src/user/schemas/user.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Transaction.name, schema: TransactionSchema },
       { name: Order.name, schema: OrderSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [PaymentController],
