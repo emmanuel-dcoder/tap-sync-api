a353ccaa7eac66e99dcbfbd071a341a60b846e04
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-11-25 15:01:20 +0100

    feat

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -46,7 +46,7 @@ export class UserController {
 
   @Post()
   @ApiOperation({
-    summary: 'Create indiviual, basic, or company',
+    summary: 'Create individual, basic, or company',
     description: `Account type must be either "inidividual", "basic" or "company"`,
   })
   @ApiConsumes('multipart/form-data')
