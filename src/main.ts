92f41aa6195671db55a743634c18dec158f69412
Noah Emmanuel Idachaba <58103602+codewithemmy@users.noreply.github.com>
2025-11-27 08:47:53 +0100

    feat

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 
+  //log application running
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
